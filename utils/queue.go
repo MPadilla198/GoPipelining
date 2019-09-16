d594f1d8c92693bc53f3cd314634f778862fdce5
MPadilla198 <mp7228757@gmail.com>
2019-09-16 03:06:01 -0500

    Built Queue boilerplate

--- utils/queue.go
+++ utils/queue.go
@@ -1,1 +1,47 @@
 package utils
+
+import (
+	"sync"
+)
+
+type Queue interface {
+	Push(interface{})
+	Pop() interface{}
+	Size() int
+	List() []interface{}
+}
+
+func NewQueue() Queue {
+	return nil
+}
+
+type node struct {
+	next  *node
+	value interface{}
+}
+
+type linkedList struct {
+	head *node
+	tail *node
+
+	size int
+
+	mux      sync.Mutex
+	nodePool sync.Pool
+}
+
+func (ll *linkedList) Push(val interface{}) {
+
+}
+
+func (ll *linkedList) Pop() interface{} {
+	return nil
+}
+
+func (ll *linkedList) Size() int {
+	return 0
+}
+
+func (ll *linkedList) List() []interface{} {
+	return []interface{}{}
+}
---
+++ utils/queue_test.go
@@ -0,0 +1,19 @@
+package utils
+
+import "testing"
+
+func TestQueue_Push(t *testing.T) {
+
+}
+
+func TestQueue_Pop(t *testing.T) {
+
+}
+
+func TestQueue_Size(t *testing.T) {
+
+}
+
+func TestQueue_List(t *testing.T) {
+
+}
