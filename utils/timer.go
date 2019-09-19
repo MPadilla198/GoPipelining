f054978a6be50e07d6c630824c88f7e0376c2739
MPadilla198 <mp7228757@gmail.com>
2019-09-19 15:34:16 -0500

    finished timer.go, and set up boilerplate for timer_test.go

--- utils/timer.go
+++ utils/timer.go
@@ -1,1 +1,116 @@
 package utils
+
+import (
+	"math"
+	"sync"
+	"time"
+)
+
+type Timer interface {
+	Start() func()
+	Av() float64
+	Std(float64) float64
+}
+
+func NewTimer(sampleSize int, avPlaceholder, stdPlaceholder float64) Timer {
+	return &timer{
+		sampleSize: sampleSize,
+		times:      newTimes(sampleSize),
+		mux:        sync.RWMutex{},
+		av:         avPlaceholder,
+		std:        stdPlaceholder,
+	}
+}
+
+type timer struct {
+	sampleSize int // used for cap size in *times, stays const.
+
+	times *times
+
+	mux sync.RWMutex
+	av  float64
+	std float64
+}
+
+func (t *timer) Start() (end func()) {
+	var startTime time.Time
+
+	end = func() {
+		timeLapsed := time.Since(startTime)
+
+		times, totalTime, recalculate := t.times.add(int64(timeLapsed))
+
+		if recalculate {
+			go func() {
+				newAv := float64(totalTime) / float64(t.sampleSize)
+
+				var variation float64
+				for _, v := range times {
+					variation += math.Pow(float64(v)-newAv, 2)
+				}
+
+				variation /= float64(t.sampleSize)
+
+				newStd := math.Sqrt(variation)
+
+				t.mux.Lock()
+				defer t.mux.Unlock()
+
+				t.av = newAv
+				t.std = newStd
+			}()
+		}
+	}
+
+	startTime = time.Now()
+
+	return
+}
+
+func (t *timer) Av() float64 {
+	t.mux.RLock()
+	defer t.mux.RUnlock()
+
+	return t.av
+}
+
+func (t *timer) Std(n float64) float64 {
+	t.mux.RLock()
+	defer t.mux.RUnlock()
+
+	return t.std
+}
+
+type times struct {
+	mux       sync.Mutex
+	totalTime int64
+	times     []int64
+}
+
+func newTimes(cap int) *times {
+	return &times{sync.Mutex{}, 0, make([]int64, 0, cap)}
+}
+
+// takes in new value to add
+// Returns times array, sum of times in array, and if times reached cap and has been reset
+func (t *times) add(n int64) ([]int64, int64, bool) {
+	t.mux.Lock()
+	defer t.mux.Unlock()
+
+	t.times = append(t.times, n)
+	t.totalTime += n
+
+	if len(t.times) != cap(t.times) {
+		return t.times, t.totalTime, false
+	}
+
+	// save and clear t.times[]
+	times := t.times
+	t.times = t.times[:] // Resets slice without touching underlying array
+
+	// save and clear t.totalTime
+	totalTime := t.totalTime
+	t.totalTime = 0
+
+	return times, totalTime, true
+}
--- utils/timer_test.go
+++ utils/timer_test.go
@@ -1,1 +1,19 @@
 package utils
+
+import "testing"
+
+func TestTimer_StartNewTimer(t *testing.T) {
+
+}
+
+func TestTimer_Av(t *testing.T) {
+
+}
+
+func TestTimer_Std(t *testing.T) {
+
+}
+
+func TestTimer(t *testing.T) {
+
+}
